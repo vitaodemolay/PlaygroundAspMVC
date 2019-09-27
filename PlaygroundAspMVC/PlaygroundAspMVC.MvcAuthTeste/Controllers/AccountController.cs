b820258f3e76a7a0c77ab00659c88a37f8af7039
Vitor Marcos Custódio <vitor.marcos@terra.com.br>
2019-09-26 23:06:08 -0300

    modelo de codigo para aplicar

--- PlaygroundAspMVC/PlaygroundAspMVC.MvcAuthTeste/Controllers/AccountController.cs
+++ PlaygroundAspMVC/PlaygroundAspMVC.MvcAuthTeste/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using PlaygroundAspMVC.MvcAuthTeste.Config.Authentication;
 using PlaygroundAspMVC.MvcAuthTeste.Config.Identity;
 using PlaygroundAspMVC.MvcAuthTeste.Models;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -104,6 +105,21 @@ namespace PlaygroundAspMVC.MvcAuthTeste.Controllers
         }
 
         [AllowAnonymous]
+        public ActionResult AuthGetToken(string returnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                HttpClient client = new HttpClient();
+                
+                var Token = Guid.NewGuid().ToString();
+                return RedirectToLocal(returnUrl + $"-{Token}");
+            }
+
+            return RedirectToAction("Login", "Account", new { returnUrl = $"{Request.Url.OriginalString}" });
+        }
+
+
+        [AllowAnonymous]
         public ActionResult Login(string returnUrl)
         {
             ViewBag.ReturnUrl = returnUrl;
@@ -173,3 +189,25 @@ namespace PlaygroundAspMVC.MvcAuthTeste.Controllers
         }
     }
 }
+
+
+
+
+// $"{Request.Url.Scheme}://{Request.Url.Authority}/api/token"
+
+/*
+ * var client = new RestClient("http://localhost:60047/api/token");
+ * var request = new RestRequest(Method.POST);
+ * request.AddHeader("cache-control", "no-cache");
+ * request.AddHeader("Connection", "keep-alive");
+ * request.AddHeader("Cookie", "__RequestVerificationToken=cHxCAE0YDP4U7ItyafJP5gRS9jmhwdmzNr8aOpFNwXvq62M6nCheb-QHhftusjwpExQwrXvW9Q87_ZxcwOhS7cB4womjMXTdlZ9As8VTC1Y1");
+ * request.AddHeader("Content-Length", "69");
+ * request.AddHeader("Accept-Encoding", "gzip, deflate");
+ * request.AddHeader("Host", "localhost:60047");
+ * request.AddHeader("Postman-Token", "ba55f1d6-7084-4c5f-9b6a-89fa7b6e39c3,09eeeaf8-4a7e-4420-bdd6-ea3a44d07340");
+ * request.AddHeader("Cache-Control", "no-cache");
+ * request.AddHeader("User-Agent", "PostmanRuntime/7.17.1");
+ * request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+ * request.AddParameter("undefined", "username=vitor.marcos%40gmail.com&password=123456&grant_type=password", ParameterType.RequestBody);
+ * IRestResponse response = client.Execute(request);
+*/
