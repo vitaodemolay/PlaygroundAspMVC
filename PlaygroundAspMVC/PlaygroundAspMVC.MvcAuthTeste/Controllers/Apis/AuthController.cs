8b239f1f2ebe6d3d75c4fd1292a55ad08e09d6c9
Vitor Marcos da Rosa Custodio <vitor.custodio@xpi.com.br>
2019-09-30 11:53:29 -0300

    uma controler de api para realizar logout

---
+++ PlaygroundAspMVC/PlaygroundAspMVC.MvcAuthTeste/Controllers/Apis/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using System.Web.Security;
+
+namespace PlaygroundAspMVC.MvcAuthTeste.Controllers.Apis
+{
+    public class AuthController : ApiController
+    {
+        
+        [HttpPost]
+        [Route("api/auth/logoff")]
+        public void logoff()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+}
